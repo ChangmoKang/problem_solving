027f7128ea91b7a6e4c21a68c5e658423d5161d8
ChangmoKang <dkssudckdah@gmail.com>
2019-12-24 11:51:48 +0900

    [Python] 백조의 호수
    물과 맞닿아 있는 빙하를 찾은 후 visited 행렬을 이용하여 언제 빙하가 녹는지를 저장함.
    만들어진 visited를 이용하여 binary search와 bfs를 수행함.

--- BOJ/3197.py
+++ BOJ/3197.py
@@ -1,32 +1,32 @@
 import sys
 sys.stdin = open('input/3197.txt')
+input = sys.stdin.readline
 
 
 def find_L():
-    mark = []
+    ans = []
     for i in range(R):
         for j in range(C):
             if board[i][j] == 'L':
-                board[i][j] = '.'
-                mark.append(visited[i][j])
-                if len(mark) == 2:
-                    return mark
+                ans.append([i, j])
+                if len(ans) == 2:
+                    return ans
 
 
-def bfs():
-    S, G = L
-    q = [S]
-    V = [0]*N
-    V[S] = 1
+def bfs(k):
+    v = [[0]*C for _ in range(R)]
+    q = [s]
+    v[s[0]][s[1]] = 1
     while q:
         qs, q = q, []
-        for FROM in qs:
-            for TO in adj[FROM]:
-                if not V[TO]:
-                    if TO == G:
+        for r, c in qs:
+            for x in range(4):
+                rr, cc = r + dr[x], c + dc[x]
+                if 0 <= rr < R and 0 <= cc < C and not v[rr][cc] and visited[rr][cc] <= k:
+                    v[rr][cc] = 1
+                    if [rr, cc] == g:
                         return True
-                    V[TO] = 1
-                    q.append(TO)
+                    q.append([rr, cc])
     return False
 
 
@@ -35,56 +35,53 @@ dc = [0, 0, -1, 1]
 R, C = map(int, input().split())
 board = [list(input()) for _ in range(R)]
 
-
-melt = []
-visited = [[-1]*C for _ in range(R)]
-index = 0
+# Store glaciers adjacent on water
+melt = set()
+visited = [[0]*C for _ in range(R)]
 for i in range(R):
     for j in range(C):
-        if board[i][j] == '.' and visited[i][j] == -1:
-            melt.append(set())
-            visited[i][j] = index
+        if board[i][j] in '.L' and not visited[i][j]:
+            visited[i][j] = 1
             q = [[i, j]]
             while q:
                 qs, q = q, []
                 for r, c in qs:
                     for x in range(4):
-                        rr = r + dr[x]
-                        cc = c + dc[x]
-                        if 0 <= rr < R and 0 <= cc < C and visited[rr][cc] == -1:
-                            visited[rr][cc] = index
+                        rr, cc = r + dr[x], c + dc[x]
+                        if 0 <= rr < R and 0 <= cc < C and not visited[rr][cc]:
+                            visited[rr][cc] = 1
                             if board[rr][cc] == 'X':
-                                melt[index].add((rr, cc))
+                                melt.add((rr, cc))
+                                visited[rr][cc] = 2
                             else:
                                 q.append([rr, cc])
-            index += 1
 
-L = find_L()
-# print(L)
+# Save melting point by days
+days = 2
+while melt:
+    days += 1
+    target, melt = melt, set()
+    for r, c in target:
+        for x in range(4):
+            rr, cc = r + dr[x], c + dc[x]
+            if 0 <= rr < R and 0 <= cc < C and not visited[rr][cc]:
+                visited[rr][cc] = days
+                if board[rr][cc] == 'X':
+                    melt.add((rr, cc))
 
-result = 0
-N = len(melt)
-adj = [[] for _ in range(N)]
-while True:
-    target, melt = melt, [set() for _ in range(N)]
-    for i in range(N):
-        for r, c in target[i]:
-            for x in range(4):
-                rr = r + dr[x]
-                cc = c + dc[x]
-                if 0 <= rr < R and 0 <= cc < C:
-                    if visited[rr][cc] == -1:
-                        visited[rr][cc] = i
-                        if board[rr][cc] == 'X':
-                            melt[i].add((rr, cc))
-                    else:
-                        j = visited[rr][cc]
-                        if (i != j) and j not in adj[i]:
-                            adj[i].append(j)
-    
-    # print(adj)
-    result += 1
-    if bfs():
-        break
+# Find swan
+s, g = find_L()
 
-print(result)
+# Binary search
+min_day = 2
+max_day = days - 1
+
+while min_day <= max_day:
+    mid = int((min_day + max_day)/2)
+
+    if bfs(mid):
+        max_day = mid - 1
+    else:
+        min_day = mid + 1
+
+print(min_day - 1)
