64c3c3d956e6b2752c84ea6df19f7cdf59e0e528
ChangmoKang <dkssudckdah@gmail.com>
2020-01-29 15:33:19 +0900

    [Python] 연구소 3
    조합으로 가능한 바이러스의 위치를 잡아주고 BFS를 이용하여 최소 확산 시간을 구하였습니다.

--- BOJ/17142.py
+++ BOJ/17142.py
@@ -2,60 +2,67 @@ import sys
 sys.stdin = open('input/17142.txt')
 
 
+def init():
+    global empty
+    for r in range(N):
+        for c in range(N):
+            if board[r][c] == 0:
+                empty += 1
+            elif board[r][c] == 2:
+                virus.append([r, c])
+
+
 def check(count, start):
     global result
     if count == K:
-        q = [virus[arr[w]][:] for w in range(K)]
-        copied_visited = [visited[w][:] for w in range(N)]
-        for r, c in q:
-            copied_visited[r][c] = 1
+        active_virus = [virus[index] for index in active_virus_index]
+
+        visited = [[0]*N for _ in range(N)]
+        for r, c in active_virus:
+            visited[r][c] = 1
+
         time = 0
-        room_cnt = 0
-        while q:
+        occupied = 0
+        while active_virus:
             time += 1
-            contents, q = q, []
-            for r, c in contents:
+
+            if time > result:
+                return
+
+            qs, active_virus = active_virus, []
+            for r, c in qs:
                 for x in range(4):
-                    rr = r + dr[x]
-                    cc = c + dc[x]
-                    if 0 <= rr < N and 0 <= cc < N and not copied_visited[rr][cc] and board[rr][cc] != 1:
-                        copied_visited[rr][cc] = 1
+                    rr, cc = r + dr[x], c + dc[x]
+                    if 0 <= rr < N and 0 <= cc < N and board[rr][cc] != 1 and not visited[rr][cc]:
+                        visited[rr][cc] = 1
                         if board[rr][cc] == 0:
-                            room_cnt += 1
-                        if room_cnt == room:
-                            if result > time:
-                                result = time
-                                return
-                        q.append([rr, cc])
+                            occupied += 1
+                        active_virus.append([rr, cc])
+
+            if empty == occupied:
+                if result > time:
+                    result = time
+                    return
     else:
         for i in range(start, len(virus)):
-            arr[count] = i
+            active_virus_index[count] = i
             check(count + 1, i + 1)
-            arr[count] = 0
+            active_virus_index[count] = None
 
 
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 N, K = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
-visited = [[0]*N for _ in range(N)]
 
+empty = 0
 virus = []
-room = 0
-for i in range(N):
-    for j in range(N):
-        if board[i][j] == 2:
-            virus.append([i, j])
-        elif board[i][j] == 1:
-            visited[i][j] = 1
-        else:
-            room += 1
+init()
 
-if room:
+if empty == 0:
+    print(0)
+else:
     result = float('inf')
-    arr = [0]*K
-    v = [0]*len(virus)
+    active_virus_index = [None]*K
     check(0, 0)
     print(result) if result != float('inf') else print(-1)
-else:
-    print(0)
