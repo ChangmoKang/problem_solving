8dec011eb746ca87b47b1dda47a529da1d15794d
ChangmoKang <dkssudckdah@gmail.com>
2020-02-01 00:05:50 +0900

    [Python] 연구소
    조합 + BFS, 한 칸씩 움직이는 로직을 연습해보았습니다.

--- BOJ/14502.py
+++ BOJ/14502.py
@@ -2,54 +2,72 @@ import sys
 sys.stdin = open('input/14502.txt')
 
 
-def check(r, c, count):
+def init():
+    global empty
+    for r in range(R):
+        for c in range(C):
+            if board[r][c] == 0:
+                empty += 1
+            elif board[r][c] == 2:
+                virus.append([r, c])
+
+    for r in range(R):
+        for c in range(C):
+            if board[r][c] == 0:
+                return check(0, r, c)
+
+
+def check(count, i, j):
     global result
     if count == 3:
-        c_board = [board[w][:] for w in range(R)]
-        q = [virus[w][:] for w in range(len(virus))]
-        sub_result = room
+        visited = [[0]*C for _ in range(R)]
+        for r, c in virus:
+            visited[r][c] = 1
+
+        room = empty
+        q = [v for v in virus]
         while q:
-            contents, q = q, []
-            for r, c in contents:
+            qs, q = q, []
+            for r, c in qs:
                 for x in range(4):
-                    rr = r + dr[x]
-                    cc = c + dc[x]
-                    if 0 <= rr < R and 0 <= cc < C and not c_board[rr][cc]:
-                        c_board[rr][cc] = 2
-                        sub_result -= 1
+                    rr, cc = r + dr[x], c + dc[x]
+                    if 0 <= rr < R and 0 <= cc < C and board[rr][cc] != 1 and not visited[rr][cc]:
+                        visited[rr][cc] = 1
+                        room -= 1
                         q.append([rr, cc])
-        if result < sub_result:
-            result = sub_result
+
+        if room > result:
+            result = room
     else:
         while True:
-            if c == C - 1:
-                r += 1
-                c = 0
-            else:
-                c += 1
-            if r == R:
+            if i == R:
                 break
 
-            if board[r][c] == 0:
-                board[r][c] = 1
-                check(r, c, count + 1)
-                board[r][c] = 0
+            if board[i][j] == 0:
+                board[i][j] = 1
+
+                if j == C - 1:
+                    check(count + 1, i + 1, 0)
+                else:
+                    check(count + 1, i, j + 1)
                 
+                board[i][j] = 0
+
+            if j == C - 1:
+                i += 1
+                j = 0
+            else:
+                j += 1
+
 
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 R, C = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(R)]
 
+empty = -3
 virus = []
-room = -3
-for i in range(R):
-    for j in range(C):
-        if board[i][j] == 0:
-            room += 1
-        elif board[i][j] == 2:
-            virus.append([i, j])
 result = 0
+init()
 
-check(0, -1, 0)
 print(result)
