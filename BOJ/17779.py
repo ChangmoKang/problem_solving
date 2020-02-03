74a93168b9b6aa88ae43ea99406e63b94ca14fd4
ChangmoKang <dkssudckdah@gmail.com>
2020-02-04 02:19:31 +0900

    [Python] 게리맨더링 2
    대각선 방향 인덱싱 문제, BFS를 왼쪽 상단, 하단, 오른쪽 상단, 하단에서 시작하여 영역을 구분하였습니다.

--- BOJ/17779.py
+++ BOJ/17779.py
@@ -2,87 +2,78 @@ import sys
 sys.stdin = open('input/17779.txt')
 
 
-def check(r, c, left, right):
-    global result
-    # 가운데
-    for x in range(left):
-        visited[r + x][c - x] = 3
-    for xx in range(right):
-        visited[r + left + xx][c - left + xx] = 3
+def bfs(s, num):
+    visited[s[0]][s[1]] = num
+    value[num] += board[s[0]][s[1]]
 
-    for x in range(right):
-        visited[r + x][c + x] = 3
-    for xx in range(left):
-        visited[r + right + xx][c + right - xx] = 3
-
-    visited[r + left + right][c - left + right] = 3
-
-
-    sum_list = [0,0,0,0]
-
-    # 위로
-    for x in range(r):
-        visited[x][c] = 1
-        sum_list[0] += board[x][c]
-
-    # 왼쪽
-    for x in range(c - left):
-        visited[r + left][x] = 4
-        sum_list[2] += board[r + left][x]
-
-    # 오른쪽
-    for x in range(c + right + 1, N):
-        visited[r + right][x] = 2
-        sum_list[1] += board[r + right][x]
-
-    # 아래
-    for x in range(r + left + right + 1, N):
-        visited[x][c - left + right] = 5
-        sum_list[3] += board[x][c - left + right]
-
-    number = [1, 2, 4, 5]
-    number_idx = 0
-    for r in [0, N -1]:
-        for c in [0, N - 1]:
-            visited[r][c] = number[number_idx]
-            sum_list[number_idx] += board[r][c]
-            q = [[r, c]]
-            while q:
-                el = q.pop(0)
-                for idx in range(4):
-                    rr = el[0] + dr[idx]
-                    cc = el[1] + dc[idx]
-                    if 0 <= rr < N and 0 <= cc < N and not visited[rr][cc]:
-                        visited[rr][cc] = number[number_idx]
-                        q.append([rr, cc])
-                        sum_list[number_idx] += board[rr][cc]
-            number_idx += 1
-
-    sum_list.append(total - sum(sum_list))
-
-    sub_result = max(sum_list) - min(sum_list)
-    if result > sub_result:
-        result = sub_result
+    q = [s]
+    while q:
+        qs, q = q, []
+        for r, c in qs:
+            for x in range(4):
+                rr, cc = r + dr[x], c + dc[x]
+                if 0 <= rr < N and 0 <= cc < N and not visited[rr][cc]:
+                    visited[rr][cc] = num
+                    q.append([rr, cc])
+                    value[num] += board[rr][cc]
 
 
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
-
 N = int(input())
 board = [list(map(int, input().split())) for _ in range(N)]
-visited = [[0]*N for _ in range(N)]
-total = sum([sum(board[i]) for i in range(N)])
 result = float('inf')
 
-for i in range(N - 2):
-    for j in range(1, N - 1):
-        for d1 in range(1, N):
-            if i + d1 > N - 1 or j - d1 < 0:
-                break
-            for d2 in range(1, N):
-                if i + d2 > N - 1 or j + d2 > N - 1 or i + d1 + d2 > N - 1:
-                    break
-                visited = [[0]*N for _ in range(N)]
-                check(i, j, d1, d2)
+for r in range(N - 1):
+    for c in range(1, N - 1):
+        for d1 in range(1, c + 1):
+            for d2 in range(1, N - c):
+                if r + d1 < N and r + d2 < N and r + d1 + d2 < N:
+                    visited = [[0]*N for _ in range(N)]
+                    value = [0]*6
+                    for index in range(d1):
+                        visited[r + index][c - index] = 5
+                        visited[r + d2 + index][c + d2 - index] = 5
+                        value[5] += board[r + index][c - index]
+                        value[5] += board[r + d2 + index][c + d2 - index]
+                    for index in range(d2):
+                        visited[r + index][c + index] = 5
+                        visited[r + d1 + index][c - d1 + index] = 5
+                        value[5] += board[r + index][c + index]
+                        value[5] += board[r + d1 + index][c - d1 + index]
+                    value[5] -= board[r][c]
+                    visited[r + d1 + d2][c - d1 + d2] = 5
+                    value[5] += board[r + d1 + d2][c - d1 + d2]
+
+                    for index in range(r):
+                        visited[index][c] = 1
+                        value[1] += board[index][c]
+
+                    for index in range(c - d1):
+                        visited[r + d1][index] = 3
+                        value[3] += board[r + d1][index]
+
+                    for index in range(c + d2 + 1, N):
+                        visited[r + d2][index] = 2
+                        value[2] += board[r + d2][index]
+
+                    for index in range(r + d1 + d2 + 1, N):
+                        visited[index][c - d1 + d2] = 4
+                        value[4] += board[index][c - d1 + d2]
+
+                    bfs([0, 0], 1)
+                    bfs([0, N - 1], 2)
+                    bfs([N - 1, 0], 3)
+                    bfs([N - 1, N - 1], 4)
+
+                    for rr in range(N):
+                        for cc in range(N):
+                            if visited[rr][cc] == 0:
+                                value[5] += board[rr][cc]
+                    
+                    value.pop(0)
+                    tmp = max(value) - min(value)
+                    if result > tmp:
+                        result = tmp
 
 print(result)
