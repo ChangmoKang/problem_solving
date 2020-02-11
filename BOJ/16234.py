aa7d2e982586d9dca8be268409bb10e1c04fdcc7
ChangmoKang <dkssudckdah@gmail.com>
2020-02-11 17:08:23 +0900

    [Python] 인구 이동
    BFS 문제
    연결될 수 있는 마을을 bfs를 사용하여 indexing하고
    전체 마을을 다 탐색하였을 때 indexing이 같은 마을에 같은 값을 할당하였습니다.

--- BOJ/16234.py
+++ BOJ/16234.py
@@ -2,22 +2,25 @@ import sys
 sys.stdin = open('input/16234.txt')
 
 
-def bfs(init):
-    save = [init]
-    q = [init]
+def bfs():
+    visited[i][j] = num
+    q = [[i, j, board[i][j]]]
+
+    cnt = 1
+    total = board[i][j]
+
     while q:
-        r, c = q.pop(0)
-        for x in range(4):
-            rr = r + dr[x]
-            cc = c + dc[x]
-            if 0 <= rr < N and 0 <= cc < N and not visited[rr][cc] and L <= abs(board[r][c] - board[rr][cc]) <= R:
-                visited[rr][cc] = number
-                save.append([rr, cc])
-                q.append([rr, cc])
-    if len(save) > 1:
-        union = int(sum([board[s[0]][s[1]] for s in save])/len(save))
-        for r, c in save:
-            board[r][c] = union
+        qs, q = q, []
+        for r, c, population in qs:
+            for x in range(4):
+                rr, cc = r + dr[x], c + dc[x]
+                if 0 <= rr < N and 0 <= cc < N and not visited[rr][cc]:
+                    if L <= abs(population - board[rr][cc]) <= R:
+                        visited[rr][cc] = num
+                        total += board[rr][cc]
+                        cnt += 1
+                        q.append([rr, cc, board[rr][cc]])
+    return int(total / cnt)
 
 
 dr = [-1, 1, 0, 0]
@@ -25,20 +28,24 @@ dc = [0, 0, -1, 1]
 N, L, R = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 
-
-result = 0
+t = 0
 while True:
+    num = 1
     visited = [[0]*N for _ in range(N)]
-    number = 1
+    s = []
     for i in range(N):
         for j in range(N):
             if not visited[i][j]:
-                visited[i][j] = number
-                bfs([i, j])
-                number += 1
-    if number == N ** 2 + 1:
+                s.append(bfs())
+                num += 1
+    
+    for i in range(N):
+        for j in range(N):
+            board[i][j] = s[visited[i][j] - 1]
+    
+    if num == N*N + 1:
         break
 
-    result += 1
+    t += 1
 
-print(result)
+print(t)
