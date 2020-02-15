c147901b52c2c4024905836640f7e2892735e8be
ChangmoKang <dkssudckdah@gmail.com>
2020-02-16 00:02:10 +0900

    [Python] 배상 비용 최소화
    heapq를 사용하여 갱신되는 최소값을 찾았습니다.

--- PG/코딩테스트_대비반/수준_파악용_테스트_3.py
+++ PG/코딩테스트_대비반/수준_파악용_테스트_3.py
@@ -1,55 +1,19 @@
+# 배상 비용 최소화
+import heapq
+
 def solution(N, works):
-    while N:
-        works.sort(reverse=True)
-        max_value = works[0]
-        min_value = works[-1]
-
-        if max_value == min_value:
-            if N >= len(works):
-                q = N // len(works)
-                works = [work - q for work in works]
-                N -= q * len(works)
-
-            for i in range(N):
-                works[i] -= 1
-            N = 0
-
+    works = [-work for work in works]
+    heapq.heapify(works)
+    
+    for _ in range(N):
+        if works[0] == 0:
+            return 0
         else:
-            for idx in range(len(works)):
-                work = works[idx]
-                if work < max_value:
-                    diff = max_value - work
-                    for _ in range(diff):
-                        if not N:
-                            break
+            target_value = heapq.heappop(works)
+            target_value += 1
+            heapq.heappush(works, target_value)
 
-                        for i in range(idx):
-                            if N:
-                                works[i] -= 1
-                                N -= 1
-                            else:
-                                break
-                    break
-
-    for idx, work in enumerate(works):
-        if work < 0:
-            works[idx] = 0
-
-    if max(works):
-        dic = {}
-        for work in works:
-            if work not in dic:
-                dic[work] = 1
-            else:
-                dic[work] += 1
-
-        result = 0
-        for key, value in dic.items():
-            result += value * pow(key, 2)
-        return result
-
-    else:
-        return 0
+    return sum(work**2 for work in works)
 
 
 if __name__ == "__main__":
