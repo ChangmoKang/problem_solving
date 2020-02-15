b5c969084ecf9ef3875d3c9bad5185d7f04cd237
ChangmoKang <dkssudckdah@gmail.com>
2020-02-16 00:01:41 +0900

    [Python] 스킬트리
    str의 startswith() method를 사용하여 검사하였습니다.

--- PG/코딩테스트_대비반/수준_파악용_테스트_2.py
+++ PG/코딩테스트_대비반/수준_파악용_테스트_2.py
@@ -1,21 +1,13 @@
+# 스킬트리
 def solution(skill, skill_trees):
     answer = 0
-    word_to_idx = {word: idx for idx, word in enumerate(skill)}
-    A = set(skill)
     for skill_tree in skill_trees:
-        skill_tree = list(skill_tree)
-        B = set(skill_tree)
-        for el in B - A:
-            skill_tree.remove(el)
+        tree = ''
+        for s in skill_tree:
+            if s in skill:
+                tree += s
         
-        flag = 0
-        for idx in range(len(skill_tree)):
-            skill_tree[idx] = word_to_idx[skill_tree[idx]]
-            if idx != skill_tree[idx]:
-                flag = 1
-                break
-        
-        if not flag:
+        if skill.startswith(tree):
             answer += 1
 
     return answer
