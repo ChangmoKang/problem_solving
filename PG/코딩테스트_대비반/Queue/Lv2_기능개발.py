67c4b617b83d8b6ef9b6e3da51e58aedb271befe
ChangmoKang <dkssudckdah@gmail.com>
2020-02-18 16:01:22 +0900

    Queue

--- PG/코딩테스트_대비반/Queue/Lv2_기능개발.py
+++ PG/코딩테스트_대비반/Queue/Lv2_기능개발.py
@@ -1,8 +1,10 @@
+from math import ceil
+
 def solution(progresses, speeds):
     answer = []
     
     N = len(progresses)
-    remains = [(100 - p)//s + 1 if (100 - p) % s else (100 - p)//s for p, s in zip(progresses, speeds)]
+    remains = [ceil((100 - p) / s) for p, s in zip(progresses, speeds)]
 
     front = 0
     for index in range(len(remains)):
