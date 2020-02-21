fc20990af28a2ec6b3e3dacc7faca6ebd28d8298
ChangmoKang <dkssudckdah@gmail.com>
2020-02-22 00:03:35 +0900

    [Python] 감시
    완전 탐색 문제
    line.14, line.31 ~ line.33: blind_spot을 제거한 후 복구할 때 방문한 곳을 저장해 두고 그 곳만 복구시켜야 한다.

--- BOJ/15683.py
+++ BOJ/15683.py
@@ -1,57 +1,61 @@
 import sys
 sys.stdin = open('input/15683.txt')
 
+WATCHED = 7
 
-def check(count):
-    global result, room
-    if count == len(cctvs):
-        if result > room:
-            result = room
+def check(index, blind_spot):
+    global result
+    if index == len(cctv):
+        if result > blind_spot:
+            result = blind_spot
     else:
-        for idxs in ways[cctvs[count][2]]:
+        r, c, k = cctv[index]
+        for dirs in monitor[k]:
             visited = []
-            for idx in idxs:
-                r, c, d = cctvs[count]
+            for dir in dirs:
+                rr, cc = r, c
                 while True:
-                    r += dr[idx]
-                    c += dc[idx]
+                    rr += dr[dir]
+                    cc += dc[dir]
 
-                    if not (0 <= r < R and 0 <= c < C) or board[r][c] == 6:
+                    if not (0 <= rr < R and 0 <= cc < C and board[rr][cc] != 6):
                         break
 
-                    if board[r][c] == 0:
-                        board[r][c] = d
-                        visited.append([r, c])
-                        room -= 1
-            check(count + 1)
-            for r, c in visited:
-                board[r][c] = 0
-                room += 1
+                    if board[rr][cc] == 0:
+                        board[rr][cc] = WATCHED
+                        visited.append([rr, cc])
+                        blind_spot -= 1
+                    
+            check(index + 1, blind_spot)
 
+            for rr, cc in visited:
+                board[rr][cc] = 0
+                blind_spot += 1
 
-ways = {
-    1: [[0], [1], [2], [3]],
-    2: [[0, 1], [2, 3]],
-    3: [[0, 3], [1, 3], [1, 2], [0, 2]],
-    4: [[0, 2, 3], [0, 1, 3], [1, 2, 3], [0, 1, 2]],
-    5: [[0, 1, 2, 3]]
-}
 
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 
+monitor = {
+    1: [[0], [1], [2], [3]],
+    2: [[0, 1], [2, 3]],
+    3: [[0, 3], [1, 2], [2, 0], [3, 1]],
+    4: [[0, 3, 1], [1, 2, 0], [2, 0, 3], [3, 1, 2]],
+    5: [[0, 1, 2, 3]]
+}
+
 R, C = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(R)]
 
-room = 0
-cctvs = []
+max_blind_spot = 0
+cctv = []
 for i in range(R):
     for j in range(C):
         if board[i][j] == 0:
-            room += 1
+            max_blind_spot += 1
         elif 1 <= board[i][j] <= 5:
-            cctvs.append([i, j, board[i][j]])
+            cctv.append([i, j, board[i][j]])
 
-result = room
-check(0)
+result = max_blind_spot
+check(0, max_blind_spot)
 print(result)
