4719e8ca31525c86890077394ef2ccbc81a28987
ChangmoKang <dkssudckdah@gmail.com>
2020-02-28 10:49:37 +0900

    [Python] 예산
    이진탐색 문제
    처음에 l을 budgets의 최솟값으로 잡아서 시간이 오래 걸린 문제

---
+++ PG/코딩테스트_대비반/Search/Lv3_예산.py
@@ -0,0 +1,20 @@
+def solution(budgets, M):
+    if M >= sum(budgets):
+        return max(budgets)
+
+    l, r = 1, max(budgets)
+    
+    while l <= r:
+        m = int((l + r)/2)
+        
+        temp_budgets = sum(m if budget > m else budget for budget in budgets)
+        
+        if temp_budgets == M:
+            return m
+        
+        if temp_budgets > M:
+            r = m - 1
+        else:
+            l = m + 1
+
+    return r
