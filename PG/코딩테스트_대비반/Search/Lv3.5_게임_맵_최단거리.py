5f1c98be7b9224f8da9003a7acd6d214541db16b
ChangmoKang <dkssudckdah@gmail.com>
2020-02-29 00:29:38 +0900

    [Python] 게임 맵 최단거리
    단방향 bfs에서 양방향 bfs로 구현해보았습니다.
    길이 끊기는 경우 빠르게 -1을 return할 수 있을 것 같아서 시도해보았습니다.

--- PG/코딩테스트_대비반/Search/Lv3.5_게임_맵_최단거리.py
+++ PG/코딩테스트_대비반/Search/Lv3.5_게임_맵_최단거리.py
@@ -1,22 +1,30 @@
-from collections import deque
-
-ROUTE = 1
-DELTAS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 def solution(maps):
+    FROM_START, FROM_END = 0, 1
+    ROUTE = 1
+    DELTAS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     N, M = len(maps), len(maps[0])
     
-    visited = [[0]*M for _ in range(N)]
-    visited[0][0] = 1
+    visited = [[None]*M for _ in range(N)]
+    visited[0][0] = (1, FROM_START)
+    visited[N - 1][M - 1] = (1, FROM_END)
     
-    q = deque([[0, 0]])
+    q = [[0, 0, FROM_START], [N - 1, M - 1, FROM_END]]
     while q:
-        r, c = q.popleft()
+        flag = [False]*2
+
+        target_same_depth_q , q = q, [] # 같은 depth를 조사하기 위함 >> 이렇게 해야 위의 flag가 의미가 있음
         
-        for dr, dc in DELTAS:
-            next_r, next_c = r + dr, c + dc
-            if 0 <= next_r < N and 0 <= next_c < M and not visited[next_r][next_c] and maps[next_r][next_c] == ROUTE:
-                visited[next_r][next_c] = visited[r][c] + 1
-                if (next_r, next_c) == (N - 1, M - 1):
-                    return visited[next_r][next_c]
-                q.append([next_r, next_c])
-    return -1
+        for r, c, k in target_same_depth_q:
+            for dr, dc in DELTAS:
+                next_r, next_c = r + dr, c + dc
+                if 0 <= next_r < N and 0 <= next_c < M and maps[next_r][next_c] == ROUTE:
+                    if visited[next_r][next_c] is not None:
+                        if visited[next_r][next_c][-1] != k:
+                            return visited[next_r][next_c][0] + visited[r][c][0]
+                    else:
+                        flag[k] = True # 길이 있는 경우
+                        visited[next_r][next_c] = (visited[r][c][0] + 1, k)
+                        q.append([next_r, next_c, k])
+        
+        if flag != [True]*2: # 한쪽 이상에서 길이 막힌 경우 -1 return
+            return -1
