586c35bf389423eaaafe8738daa6e31ac183aa85
ChangmoKang <dkssudckdah@gmail.com>
2020-02-29 00:27:40 +0900

    [Python] 주사위 게임
    reduce 함수를 이용해서 리스트의 원소 곱을 구해봤습니다.

--- PG/코딩테스트_대비반/Search/Lv2_주사위_게임.py
+++ PG/코딩테스트_대비반/Search/Lv2_주사위_게임.py
@@ -1,18 +1,20 @@
-def solution(monster, S1, S2, S3):
+from functools import reduce
+
+def solution(monster, *dices):
     def check(count, loc):
         nonlocal empty_loc_cases
         if count == 3:
             if loc in monster_loc:
                 empty_loc_cases -= 1
         else:
-            for i in range(S[count]):
+            for i in range(dices[count]):
                 check(count + 1, loc + (i + 1))  # 주사위가 1부터 시작하므로 1을 더해줌
 
 
-    S = [S1, S2, S3]
     monster_loc = set(monster)
     
-    all_cases = eval("*".join(map(str, S)))
+    # all_cases = eval("*".join(map(str, dices)))
+    all_cases = reduce(lambda a, b: a*b, dices)
     empty_loc_cases = all_cases
     
     check(0, 1)
