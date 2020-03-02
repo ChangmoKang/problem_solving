bb44b4e0e9dfcc13fcc8afff169cc0445c9edeaa
ChangmoKang <dkssudckdah@gmail.com>
2020-03-03 00:27:04 +0900

    [Python] 등산로 조성
    완전탐색문제(BFS, DFS)
    문제에 주어진 조건에 따라 구현하였습니다.

--- SWEA/1949.py
+++ SWEA/1949.py
@@ -2,51 +2,47 @@ import sys
 sys.stdin = open('input/1949.txt')
 
 
-def check(count, status, op):
+def check(r, c, x, sub_result):
     global result
-    if count > result:
-        result = count
-    r = status[0]
-    c = status[1]
-    for i in range(4):
-        rr = r + dr[i]
-        cc = c + dc[i]
-        if 0 <= rr < N and 0 <= cc < N:
-            if not visited[rr][cc]:
-                visited[rr][cc] = 1
-                if board[rr][cc] < board[r][c]:
-                    check(count + 1, [rr, cc], op)
-                else:
-                    if op:
-                        for k in range(1, K + 1):
+
+    if sub_result > result:
+        result = sub_result
+
+    for dr, dc in DELTAS:
+        rr, cc = r + dr, c + dc
+        if 0 <= rr < N and 0 <= cc < N and not visited[rr][cc]:
+            visited[rr][cc] = 1
+            if board[r][c] > board[rr][cc]:
+                check(rr, cc, x, sub_result + 1)
+            else:
+                if x:
+                    for k in range(1, K + 1):
+                        if board[r][c] > board[rr][cc] - k:
                             board[rr][cc] -= k
-                            if board[rr][cc] < board[r][c]:
-                                check(count + 1, [rr, cc], 0)
+                            check(rr, cc, 0, sub_result + 1)
                             board[rr][cc] += k
-                visited[rr][cc] = 0
+            visited[rr][cc] = 0
 
 
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
+DELTAS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 for tc in range(1, int(input()) + 1):
     N, K = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(N)]
 
-    max_height = 0
+    max_value = 0
+    max_height = []
     for i in range(N):
         for j in range(N):
-            if board[i][j] > max_height:
-                st = [[i, j]]
-                max_height = board[i][j]
-            elif board[i][j] == max_height:
-                st.append([i, j])
-
+            if board[i][j] > max_value:
+                max_height = [[i, j]]
+                max_value = board[i][j]
+            elif board[i][j] == max_value:
+                max_height.append([i, j])
+    
     result = 0
-
-    visited = [[0]*N for _ in range(N)]
-    for s in st:
-        visited[s[0]][s[1]] = 1
-        check(1, s, 1)
-        visited[s[0]][s[1]] = 0
-
-    print(f"#{tc} {result}")
+    for i, j in max_height:
+        visited = [[0]*N for _ in range(N)]
+        visited[i][j] = 1
+        check(i, j, 1, 1)
+    
+    print("#{} {}".format(tc, result))
