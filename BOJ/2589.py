baf66f8933f1c1ab79205c50f24a13f187eefa06
ChangmoKang <dkssudckdah@gmail.com>
2020-03-04 21:34:55 +0900

    [Python] 보물섬
    데이터 추가로 인해 풀었던 문제가 틀렸다고 하여 BFS를 다시 검토한 뒤에 제출
    시작점을 포함하지 않는 거리이기 때문에 root를 0으로 설정하고 했었어야 했다.

--- BOJ/2589.py
+++ BOJ/2589.py
@@ -6,20 +6,21 @@ def bfs(start):
     global result
 
     q = [start]
-    sub_result = 0
+    route = -1
     while q:
-        sub_result += 1
+        route += 1
+
         contents, q = q, []
         for r, c in contents:
             for x in range(4):
                 rr = r + dr[x]
                 cc = c + dc[x]
                 if 0 <= rr < N and 0 <= cc < M and not visited[rr][cc] and board[rr][cc] == 'L':
-                    visited[rr][cc] = 1
+                    visited[rr][cc] = True
                     q.append([rr, cc])
 
-    if sub_result - 1 > result:
-        result = sub_result - 1
+    if route > result:
+        result = route
 
 
 dr = [-1, 1, 0, 0]
@@ -31,8 +32,50 @@ result = 0
 for i in range(N):
     for j in range(M):
         if board[i][j] == 'L':
-            visited = [[0]*M for _ in range(N)]
-            visited[i][j] == 1
+            visited = [[False]*M for _ in range(N)]
+            visited[i][j] = True
             bfs([i, j])
-
 print(result)
+
+
+"""
+>>> visited에 depth를 저장하는 방식
+
+import sys
+sys.stdin = open('input/2589.txt')
+
+
+def bfs(start):
+    global result
+
+    q = [start]
+    max_value = 0
+    while q:
+        contents, q = q, []
+        for r, c in contents:
+            for x in range(4):
+                rr = r + dr[x]
+                cc = c + dc[x]
+                if 0 <= rr < N and 0 <= cc < M and visited[rr][cc] == -1 and board[rr][cc] == 'L':
+                    visited[rr][cc] = visited[r][c] + 1
+                    max_value = max(max_value, visited[rr][cc])
+                    q.append([rr, cc])
+
+    if max_value > result:
+        result = max_value
+    
+
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+N, M = map(int, input().split())
+board = [list(input()) for _ in range(N)]
+
+result = 0
+for i in range(N):
+    for j in range(M):
+        if board[i][j] == 'L':
+            visited = [[-1]*M for _ in range(N)]
+            visited[i][j] = 0
+            bfs([i, j])
+print(result)
+"""
