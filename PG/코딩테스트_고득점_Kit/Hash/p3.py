74a54439bdd518b7d8ddb918ee8aed68c341e801
ChangmoKang <dkssudckdah@gmail.com>
2020-03-11 14:45:40 +0900

    [Python] 위장
    defaultdict로 카운트하는 코드를 정리하였고  eval을 사용하여 곱셈하였습니다.

--- PG/코딩테스트_고득점_Kit/Hash/p3.py
+++ PG/코딩테스트_고득점_Kit/Hash/p3.py
@@ -1,26 +1,14 @@
+from collections import defaultdict
+
 def solution(clothes):
-
-    def mul(List):
-        tmp = 1
-        for el in List:
-            tmp *= el
-        return tmp
-
-
-    # kind별로 name을 정리함
-    dic = {}
-    for name, kind in clothes:
-        if kind not in dic:
-            dic[kind] = [name]
-        else:
-            dic[kind].append(name)
+    clothes_sort_by_kind = defaultdict(int)
     
-    # 각각의 kind의 name의 갯수
-    names_each_kind = [len(value) + 1 for value in dic.values()]
+    for _, kind in clothes:
+        clothes_sort_by_kind[kind] += 1
+    
+    pieces = [count + 1 for count in clothes_sort_by_kind.values()]
+    return eval("*".join(str(piece) for piece in pieces)) - 1
 
-    # 경우의 수 계산
-    answer = mul(names_each_kind) - 1
-    return answer
 
 if __name__ == "__main__":
     INPUT1 = [
