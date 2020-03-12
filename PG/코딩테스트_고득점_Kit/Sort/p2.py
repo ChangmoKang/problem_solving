b3e6784d82ed817b0ba3e91366b7f825e0f842b8
ChangmoKang <dkssudckdah@gmail.com>
2020-03-13 00:41:06 +0900

    [Python] 가장 큰 수
    문자열로 이루어진 숫자들의 리스트가 정렬 될 때 어떤 기준으로 정렬 하는지 알게되었습니다.

---
+++ PG/코딩테스트_고득점_Kit/Sort/p2.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    if sum(numbers) == 0:
+        return '0'
+    
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x:x*3, reverse=True)
+    
+    return "".join(numbers)
