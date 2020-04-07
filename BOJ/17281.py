fd942c4e5be7947038b5edfe49fee757619d5736
ChangmoKang <dkssudckdah@gmail.com>
2020-04-07 14:11:21 +0900

    [Python] ⚾
    완전탐색
    permutation 중간에 값을 넣는 방법을 알게된 문제

--- BOJ/17281.py
+++ BOJ/17281.py
@@ -1,65 +1,43 @@
 import sys
+from itertools import permutations
 sys.stdin = open('input/17281.txt')
+input = sys.stdin.readline
 
-
-def check(count):
-    global result
-    if count == 9:
-        total = 0
-        current_idx = 0
-        for k in range(N):
-            out_count = 0
-            base = []
-            while True:
-                if out_count == 3:
-                    break
-
-                if board[k][order[current_idx]] == 0:
-                    out_count += 1
-                elif board[k][order[current_idx]] < 4:
-                    end_index = -1
-                    if base:
-                        for i in range(len(base)):
-                            base[i] += board[k][order[current_idx]]
-                            if base[i] >= 4:
-                                end_index = i
-
-                    if end_index != -1:
-                        base = base[end_index + 1:]
-                        total += end_index + 1
-
-                    base.append(board[k][order[current_idx]])
-                else:
-                    total += len(base) + 1
-                    base = []
-                
-                # 순서 바꾸기
-                if current_idx + 1 == 9:
-                    current_idx = 0
-                else:
-                    current_idx += 1
-
-        if total > result:
-            result = total
-    elif count == 3:
-        order[count] = 0
-        check(count + 1)
-    else:
-        for i in range(1, 9):
-            if not visited[i]:
-                visited[i] = 1
-                order[count] = i
-                check(count + 1)
-                order[count] = 0
-                visited[i] = 0
-            
 
 N = int(input())
-board = [list(map(int, input().split())) for _ in range(N)]
+inning = [list(map(int, input().split())) for _ in range(N)]
 
-order = [0]*9
-visited = [1] + [0]*8
-cnt = 0
-result = 0
-check(0)
-print(result)
+answer = 0
+
+for order in permutations(range(1, 9)):
+    order = list(order[:3]) + [0] + list(order[3:])
+
+    index = 0
+    score = 0
+    for each in inning:
+        a, b, c = 0, 0, 0
+        out = 0
+        while out < 3:
+            result = each[order[index]]
+
+            if result == 0:
+                out += 1
+            elif result == 1:
+                score += c
+                a, b, c = 1, a, b
+            elif result == 2:
+                score += (b + c)
+                a, b, c = 0, 1, a
+            elif result == 3:
+                score += (a + b + c)
+                a, b, c = 0, 0, 1
+            elif result == 4:
+                score += (a + b + c + 1)
+                a, b, c = 0, 0, 0
+
+            index = (index + 1) % 9
+
+    if score > answer:
+        answer = score
+
+print(answer)
