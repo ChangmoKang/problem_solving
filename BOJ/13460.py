36e48f54cc79ac8e269946f0409bb4b2299fbe71
ChangmoKang <dkssudckdah@gmail.com>
2020-04-14 12:32:42 +0900

    [Python] 구슬 탈출 2
    시뮬레이션 + BFS

---
+++ BOJ/13460.py
@@ -0,0 +1,435 @@
+import sys
+from itertools import product
+sys.stdin = open('input/13460.txt')
+
+
+def move(method, red, blue):
+    if method == 0:
+        if red[C] == blue[C]:
+            if red[R] < blue[R]:
+                r, c = red
+                while True:
+                    r += dr[method]
+                    c += dc[method]
+
+                    if board[r][c] == WALL:
+                        r -= dr[method]
+                        c -= dc[method]
+                        red = (r, c)
+                        break
+
+                    elif board[r][c] == EXIT:
+                        red = (r, c)
+                        break
+                
+                r, c = blue
+                while True:
+                    r += dr[method]
+                    c += dc[method]
+
+                    if board[r][c] == EXIT:
+                        blue = (r, c)
+                        break
+
+                    elif board[r][c] == WALL or (r, c) == red:
+                        r -= dr[method]
+                        c -= dc[method]
+                        blue = (r, c)
+                        break
+
+            else:
+                r, c = blue
+                while True:
+                    r += dr[method]
+                    c += dc[method]
+
+                    if board[r][c] == WALL:
+                        r -= dr[method]
+                        c -= dc[method]
+                        blue = (r, c)
+                        break
+
+                    elif board[r][c] == EXIT:
+                        blue = (r, c)
+                        break
+                
+                r, c = red
+                while True:
+                    r += dr[method]
+                    c += dc[method]
+
+                    if board[r][c] == EXIT:
+                        red = (r, c)
+                        break
+
+                    elif board[r][c] == WALL or (r, c) == blue:
+                        r -= dr[method]
+                        c -= dc[method]
+                        red = (r, c)
+                        break
+
+        else:
+            r, c = red
+            while True:
+                r += dr[method]
+                c += dc[method]
+
+                if board[r][c] == WALL:
+                    r -= dr[method]
+                    c -= dc[method]
+                    red = (r, c)
+                    break
+
+                elif board[r][c] == EXIT:
+                    red = (r, c)
+                    break
+
+            r, c = blue
+            while True:
+                r += dr[method]
+                c += dc[method]
+
+                if board[r][c] == EXIT:
+                    blue = (r, c)
+                    break
+
+                elif board[r][c] == WALL:
+                    r -= dr[method]
+                    c -= dc[method]
+                    blue = (r, c)
+                    break
+
+
+
+    elif method == 1:
+        if red[C] == blue[C]:
+            if red[R] > blue[R]:
+                r, c = red
+                while True:
+                    r += dr[method]
+                    c += dc[method]
+
+                    if board[r][c] == WALL:
+                        r -= dr[method]
+                        c -= dc[method]
+                        red = (r, c)
+                        break
+
+                    elif board[r][c] == EXIT:
+                        red = (r, c)
+                        break
+                
+                r, c = blue
+                while True:
+                    r += dr[method]
+                    c += dc[method]
+
+                    if board[r][c] == EXIT:
+                        blue = (r, c)
+                        break
+
+                    elif board[r][c] == WALL or (r, c) == red:
+                        r -= dr[method]
+                        c -= dc[method]
+                        blue = (r, c)
+                        break
+
+            else:
+                r, c = blue
+                while True:
+                    r += dr[method]
+                    c += dc[method]
+
+                    if board[r][c] == WALL:
+                        r -= dr[method]
+                        c -= dc[method]
+                        blue = (r, c)
+                        break
+
+                    elif board[r][c] == EXIT:
+                        blue = (r, c)
+                        break
+                
+                r, c = red
+                while True:
+                    r += dr[method]
+                    c += dc[method]
+
+                    if board[r][c] == EXIT:
+                        red = (r, c)
+                        break
+
+                    elif board[r][c] == WALL or (r, c) == blue:
+                        r -= dr[method]
+                        c -= dc[method]
+                        red = (r, c)
+                        break
+
+        else:
+            r, c = red
+            while True:
+                r += dr[method]
+                c += dc[method]
+
+                if board[r][c] == WALL:
+                    r -= dr[method]
+                    c -= dc[method]
+                    red = (r, c)
+                    break
+
+                elif board[r][c] == EXIT:
+                    red = (r, c)
+                    break
+
+            r, c = blue
+            while True:
+                r += dr[method]
+                c += dc[method]
+
+                if board[r][c] == WALL:
+                    r -= dr[method]
+                    c -= dc[method]
+                    blue = (r, c)
+                    break
+
+                elif board[r][c] == EXIT:
+                    blue = (r, c)
+                    break
+
+
+    elif method == 2:
+        if red[R] == blue[R]:
+            if red[C] < blue[C]:
+                r, c = red
+                while True:
+                    r += dr[method]
+                    c += dc[method]
+
+                    if board[r][c] == WALL:
+                        r -= dr[method]
+                        c -= dc[method]
+                        red = (r, c)
+                        break
+
+                    elif board[r][c] == EXIT:
+                        red = (r, c)
+                        break
+                
+                r, c = blue
+                while True:
+                    r += dr[method]
+                    c += dc[method]
+
+                    if board[r][c] == EXIT:
+                        blue = (r, c)
+                        break
+
+                    elif board[r][c] == WALL or (r, c) == red:
+                        r -= dr[method]
+                        c -= dc[method]
+                        blue = (r, c)
+                        break
+
+            else:
+                r, c = blue
+                while True:
+                    r += dr[method]
+                    c += dc[method]
+
+                    if board[r][c] == WALL:
+                        r -= dr[method]
+                        c -= dc[method]
+                        blue = (r, c)
+                        break
+
+                    elif board[r][c] == EXIT:
+                        blue = (r, c)
+                        break
+                
+                r, c = red
+                while True:
+                    r += dr[method]
+                    c += dc[method]
+
+                    if board[r][c] == EXIT:
+                        red = (r, c)
+                        break
+
+                    elif board[r][c] == WALL or (r, c) == blue:
+                        r -= dr[method]
+                        c -= dc[method]
+                        red = (r, c)
+                        break
+
+        else:
+            r, c = red
+            while True:
+                r += dr[method]
+                c += dc[method]
+
+                if board[r][c] == WALL:
+                    r -= dr[method]
+                    c -= dc[method]
+                    red = (r, c)
+                    break
+
+                elif board[r][c] == EXIT:
+                    red = (r, c)
+                    break
+
+            r, c = blue
+            while True:
+                r += dr[method]
+                c += dc[method]
+
+                if board[r][c] == WALL:
+                    r -= dr[method]
+                    c -= dc[method]
+                    blue = (r, c)
+                    break
+
+                elif board[r][c] == EXIT:
+                    blue = (r, c)
+                    break
+
+
+    else:
+        if red[R] == blue[R]:
+            if red[C] > blue[C]:
+                r, c = red
+                while True:
+                    r += dr[method]
+                    c += dc[method]
+
+                    if board[r][c] == WALL:
+                        r -= dr[method]
+                        c -= dc[method]
+                        red = (r, c)
+                        break
+
+                    elif board[r][c] == EXIT:
+                        red = (r, c)
+                        break
+                
+                r, c = blue
+                while True:
+                    r += dr[method]
+                    c += dc[method]
+
+                    if board[r][c] == EXIT:
+                        blue = (r, c)
+                        break
+
+                    elif board[r][c] == WALL or (r, c) == red:
+                        r -= dr[method]
+                        c -= dc[method]
+                        blue = (r, c)
+                        break
+
+            else:
+                r, c = blue
+                while True:
+                    r += dr[method]
+                    c += dc[method]
+
+                    if board[r][c] == WALL:
+                        r -= dr[method]
+                        c -= dc[method]
+                        blue = (r, c)
+                        break
+
+                    elif board[r][c] == EXIT:
+                        blue = (r, c)
+                        break
+                
+                r, c = red
+                while True:
+                    r += dr[method]
+                    c += dc[method]
+
+                    if board[r][c] == EXIT:
+                        red = (r, c)
+                        break
+
+                    elif board[r][c] == WALL or (r, c) == blue:
+                        r -= dr[method]
+                        c -= dc[method]
+                        red = (r, c)
+                        break
+
+        else:
+            r, c = red
+            while True:
+                r += dr[method]
+                c += dc[method]
+
+                if board[r][c] == WALL:
+                    r -= dr[method]
+                    c -= dc[method]
+                    red = (r, c)
+                    break
+
+                elif board[r][c] == EXIT:
+                    red = (r, c)
+                    break
+
+            r, c = blue
+            while True:
+                r += dr[method]
+                c += dc[method]
+
+                if board[r][c] == WALL:
+                    r -= dr[method]
+                    c -= dc[method]
+                    blue = (r, c)
+                    break
+
+                elif board[r][c] == EXIT:
+                    blue = (r, c)
+                    break
+
+    return [red, blue]
+
+
+def bfs():
+    q = [[ori_red, ori_blue]]
+    save = set()
+    save.add((ori_red, ori_blue))
+    cnt = 0
+    while q:
+        qs, q = q, []
+        for target_red, target_blue in qs:
+            if board[target_blue[R]][target_blue[C]] == EXIT:
+                continue
+
+            if board[target_red[R]][target_red[C]] == EXIT:
+                return cnt
+
+            for x in range(4):
+                new_r, new_b = move(x, target_red, target_blue)
+                if (new_r, new_b) not in save:
+                    q.append([new_r, new_b])
+                    save.add((new_r, new_b))
+
+        cnt += 1
+        if cnt == 11:
+            break
+
+    return -1
+
+
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+R, C, EMPTY, WALL, EXIT = 0, 1, '.', '#', 'O'
+
+N, M = map(int, input().split())
+board = [list(input()) for _ in range(N)]
+for r, c, in product(range(N), range(M)):
+    if board[r][c] == 'R':
+        ori_red = (r, c)
+        board[r][c] = EMPTY
+    elif board[r][c] == 'B':
+        ori_blue = (r, c)
+        board[r][c] = EMPTY
+
+print(bfs())
