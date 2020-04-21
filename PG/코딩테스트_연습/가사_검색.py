0a0ff5879ab6e3a38f6f202e833a2c669ed527f5
ChangmoKang <dkssudckdah@gmail.com>
2020-04-21 16:29:06 +0900

    Trie
    문제의 조건 중 무조건 ?가 있고 끊어져서 ?가 있지 않기 때문에
    단어의 정방향과 역방향 두개의 Trie를 만들어서 조사하였습니다.

---
+++ PG/코딩테스트_연습/가사_검색.py
@@ -0,0 +1,43 @@
+FORWARD, BACKWARD = 0, 1
+def solution(words, queries):
+    trie = {}
+
+    for word in words:
+        length = len(word)
+
+        trie.setdefault(length, ({}, {}))
+
+        target = trie[length][FORWARD]
+        for ch in word:
+            target['count'] = target.get('count', 0) + 1
+            target = target.setdefault(ch, {})
+
+        target = trie[length][BACKWARD]
+        for ch in word[::-1]:
+            target['count'] = target.get('count', 0) + 1
+            target = target.setdefault(ch, {})
+    
+    answer = []
+    for query in queries:
+        length = len(query)
+
+        if length not in trie:
+            answer.append(0)
+            continue
+
+        if query[0] == '?':
+            query = query[::-1]
+            target = trie[length][BACKWARD]
+        else:
+            target = trie[length][FORWARD]
+
+        for ch in query:
+            if ch == '?':
+                answer.append(target['count'])
+                break
+            if ch not in target:
+                answer.append(0)
+                break
+            target = target[ch]
+
+    return answer
