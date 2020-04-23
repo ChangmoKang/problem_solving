4ad44c23cb79d6e686c8597347863f97025034e1
ChangmoKang <dkssudckdah@gmail.com>
2020-04-23 11:35:57 +0900

    DFS
    BFS로도 풀이가 가능하지만 주어진 항공권을 모두 사용해야 하기 때문에 DFS로 접근하였습니다.

--- PG/코딩테스트_고득점_Kit/DFS와BFS/p4.py
+++ PG/코딩테스트_고득점_Kit/DFS와BFS/p4.py
@@ -1,51 +1,30 @@
+FROM, TO = 0, 1
 def solution(tickets):
 
-    def dfs(count, List):
-        if not result:
-            if count == len(tickets):
-               result.append(List)
-            else:
-                if List[-1] in dic:
-                    for t_i in range(len(dic[List[-1]])):
-                        t = dic[List[-1]][t_i]
-                        if not visited[List[-1]][t_i]:
-                            visited[List[-1]][t_i] = 1
-                            dfs(count + 1, List + [t])
-                            visited[List[-1]][t_i] = 0
+    def dfs(start, count, route):
+        nonlocal answer
 
+        if answer:
+            return
 
-    dic = {}
-    for f, t in tickets:
-        if f not in dic:
-            dic[f] = [t]
+        if count == N:
+            answer = route[:]
+            return
         else:
-            dic[f].append(t)
+            for i, ticket in enumerate(tickets):
+                if not visited[i] and ticket[FROM] == start:
+                    visited[i] = True
+                    route.append(ticket[TO])
+                    dfs(ticket[TO], count + 1, route)
+                    route.pop()
+                    visited[i] = False
+                    
 
-    for key in dic:
-        dic[key].sort()
+    tickets.sort(key=lambda x: x[1])
 
-    result = []
-    visited = {key: [0]*len(value) for key, value in dic.items()}
-    dfs(0, ['ICN'])
+    N = len(tickets)
+    visited = [False]*N
 
-    return result[0]
-
-
-if __name__ == "__main__":
-    INPUT = [
-        [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]],
-        [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]],
-        [["ICN", "COO"], ["ICN", "BOO"], ["COO", "ICN"], ["BOO", "DOO"]]
-    ]
-    ANSWER = [
-        ["ICN", "JFK", "HND", "IAD"],
-        ["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"],
-        ["ICN", "COO", "ICN", "BOO", "DOO"]
-    ]
-    
-    for index in range(3):
-        if ANSWER[index] == solution(INPUT[index]):
-            print(True)
-        else:
-            print(False)
-        
+    answer = []
+    dfs('ICN', 0, ['ICN'])
+    return answer
