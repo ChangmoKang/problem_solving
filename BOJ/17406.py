53e11bc6925598e50e2f9a9e4a667e87a073e992
ChangmoKang <dkssudckdah@gmail.com>
2020-04-29 13:56:53 +0900

    [Python] 배열 돌리기 4
    순열
    permutations를 이용하여 풀이하였습니다. 이전 풀이보다 4배 이상 시간이 더 소요됨.

--- BOJ/17406.py
+++ BOJ/17406.py
@@ -1,79 +1,45 @@
 import sys
+from itertools import permutations
 sys.stdin = open('input/17406.txt')
 
+dr = [0, 1, 0, -1]
+dc = [1, 0, -1, 0]
 
-def calc(r, c, N, mat):
-    for n in range(1, N + 1):
-        box1 = mat[r - n + 1][c - n]
-        box2 = None
-        for w in range(-n, n):
-            if box1:
-                box2 = mat[r - n][c + w]
-                mat[r - n][c + w] = box1
-                box1 = None
-            else:
-                box1 = mat[r - n][c + w]
-                mat[r - n][c + w] = box2
-                box2 = None
+R, C, M = map(int, input().split())
+o_board = [list(map(int, input().split())) for _ in range(R)]
+ops = []
+for _ in range(M):
+    r, c, k = map(int, input().split())
+    ops.append((r - 1, c - 1, k))
 
-        for w in range(-n, n):
-            if box1:
-                box2 = mat[r + w][c + n]
-                mat[r + w][c + n] = box1
-                box1 = None
-            else:
-                box1 = mat[r + w][c + n]
-                mat[r + w][c + n] = box2
-                box2 = None
+answer = float('inf')
+for order in permutations(ops):
+    board = [o_b[:] for o_b in o_board]
 
-        for w in range(n, -n, -1):
-            if box1:
-                box2 = mat[r + n][c +w]
-                mat[r + n][c +w] = box1
-                box1 = None
-            else:
-                box1 = mat[r + n][c +w]
-                mat[r + n][c +w] = box2
-                box2 = None
+    for r, c, k in order:
+        for l in range(1, k + 1):
+            rr = r - l
+            cc = c - l
 
-        for w in range(n, -n, -1):
-            if box1:
-                box2 = mat[r + w][c - n]
-                mat[r + w][c - n] = box1
-                box1 = None
-            else:
-                box1 = mat[r + w][c - n]
-                mat[r + w][c - n] = box2
-                box2 = None
-    return mat
+            t1 = board[rr][cc]
+            t2 = None
+            for x in range(4):
+                for _ in range(2*l):
+                    rr += dr[x]
+                    cc += dc[x]
 
+                    if t1 is not None:
+                        t2 = board[rr][cc]
+                        board[rr][cc] = t1
+                        t1 = None
+                    elif t2 is not None:
+                        t1 = board[rr][cc]
+                        board[rr][cc] = t2
+                        t2 = None
+    
+    result = min(sum(b) for b in board)
 
-def check(count, matrix):
-    global result
-    if count == K:
-        for i in range(R):
-            sub_result = sum(matrix[i])
-            if result > sub_result:
-                result = sub_result
-    else:
-        for idx in range(K):
-            if not visited[idx]:
-                visited[idx] = 1
-                copied_maxtrix = [matrix[w][:] for w in range(R)]
-                copied_maxtrix = calc(methods[idx][0] - 1, methods[idx][1] - 1, methods[idx][2], copied_maxtrix)
-                check(count + 1, copied_maxtrix)
-                visited[idx] = 0
+    if answer > result:
+        answer = result
 
-
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
-R, C, K = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(R)]
-methods = [list(map(int, input().split())) for _ in range(K)]
-
-result = float('inf')
-
-visited = [0]*K
-check(0, board)
-
-print(result)
+print(answer)
