d6f90ac8b13e26e1bce5ae9d7990ea87e5bce91d
ChangmoKang <dkssudckdah@gmail.com>
2020-04-30 23:12:30 +0900

    [Python] 벌꿀채취
    완전탐색 + 조합

--- SWEA/2115.py
+++ SWEA/2115.py
@@ -2,45 +2,53 @@ import sys
 sys.stdin = open('input/2115.txt')
 
 
-def calc(index, count, value, bucket, visited):
-    global honey
-
-    if count <= C:
-        if value > honey[index]:
-            honey[index] = value
+def calc(r, c, sub_count, sub_result, sub_pow_result):
+    global tmp
+    if sub_count == M - 1:
+        return
 
     for i in range(M):
         if not visited[i]:
-            visited[i] = 1
-            calc(index, count + bucket[i], value + bucket[i]**2, bucket, visited)
-            visited[i] = 0
+            visited[i] = True
+            if sub_result + board[r][c + i] <= C:
+                if sub_pow_result + pow_board[r][c + i] > tmp:
+                    tmp = sub_pow_result + pow_board[r][c + i]
+                calc(r, c, sub_count + 1, sub_result + board[r][c + i], sub_pow_result + pow_board[r][c + i])
+            visited[i] = False
+
+
+def check(r, c, count, result):
+    global tmp, answer
+    if count == 2:
+        if result > answer:
+            answer = result
+        return
+
+    while True:
+        if r == N:
+            break
+
+        if c + M <= N:
+            if sum(board[r][c:c+M]) <= C:
+                check(r, c + M, count + 1, result + sum(pow_board[r][c:c+M]))
+            else:
+                tmp = 0
+                calc(r, c, 0, 0, 0)
+                check(r, c + M, count + 1, result + tmp)
+        
+        if c >= N - 1:
+            r += 1
+            c = 0
+        else:
+            c += 1
 
 
 for tc in range(1, int(input()) + 1):
     N, M, C = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(N)]
+    pow_board = [[pow(board[r][c], 2) for c in range(N)] for r in range(N)]
 
-    result = 0
-
-    for i in range(N):
-        for j in range(N - (M - 1)):
-            honey = [0, 0]
-
-            bucket1 = board[i][j:j+M]
-            calc(0, 0, 0, bucket1, [0]*M)
-
-            for r in range(i, N):
-                if r == i:
-                    for c in range(j + M, N - (M - 1)):
-                        bucket2 = board[r][c:c+M]
-                        calc(1, 0, 0, bucket2, [0]*M)
-                else:
-                    for c in range(N - (M - 1)):
-                        bucket2 = board[r][c:c+M]
-                        calc(1, 0, 0, bucket2, [0]*M)
-
-            sub_result = sum(honey)
-            if sub_result > result:
-                result = sub_result
-
-    print(f"#{tc} {result}")
+    answer = 0
+    visited = [False]*M
+    check(0, 0, 0, 0)
+    print(f"#{tc} {answer}")
