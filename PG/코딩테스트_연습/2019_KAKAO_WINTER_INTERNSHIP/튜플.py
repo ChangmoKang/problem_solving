68e8f2694d77bfeea18579d938da2f3a0f903dcd
ChangmoKang <dkssudckdah@gmail.com>
2020-05-07 18:06:23 +0900

    정규표현식과 Counter를 이용한 한 줄 코딩

--- PG/코딩테스트_연습/2019_KAKAO_WINTER_INTERNSHIP/튜플.py
+++ PG/코딩테스트_연습/2019_KAKAO_WINTER_INTERNSHIP/튜플.py
@@ -1,3 +1,16 @@
+"""
+import re
+from collections import Counter
+
+
+KEY, VALUE = 0, 1
+
+
+def solution(s):
+    return list(map(int, [k for k, v in sorted(Counter(re.findall('\d+', s)).items(), key=lambda x: -x[VALUE])]))
+"""
+
+
 def solution(s):
     s = s[2:-2].split("},{")
     s.sort(key=len)
