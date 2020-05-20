90c946c5fe479202d96212efe90c024c353e5c3c
ChangmoKang <dkssudckdah@gmail.com>
2020-05-20 11:01:02 +0900

    product를 사용하여 풀이

--- PG/코딩테스트_대비반/Search/Lv2_주사위_게임.py
+++ PG/코딩테스트_대비반/Search/Lv2_주사위_게임.py
@@ -1,22 +1,16 @@
-from functools import reduce
+from itertools import product
+
 
 def solution(monster, *dices):
-    def check(count, loc):
-        nonlocal empty_loc_cases
-        if count == 3:
-            if loc in monster_loc:
-                empty_loc_cases -= 1
-        else:
-            for i in range(dices[count]):
-                check(count + 1, loc + (i + 1))  # 주사위가 1부터 시작하므로 1을 더해줌
+    monster = set(monster)
+    
+    case = eval("*".join(map(str, dices)))
+    
+    dices = [range(1, dice + 1) for dice in dices]
+    
+    answer = 0
+    for order in product(*dices):
+        if sum(order) + 1 not in monster:
+            answer += 1
 
-
-    monster_loc = set(monster)
-    
-    # all_cases = eval("*".join(map(str, dices)))
-    all_cases = reduce(lambda a, b: a*b, dices)
-    empty_loc_cases = all_cases
-    
-    check(0, 1)
-    
-    return int(empty_loc_cases/all_cases*1000)
+    return int(answer/case*1000)
