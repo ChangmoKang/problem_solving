e448970ebcaa9ad8d005a5f776afdb27051b26f8
ChangmoKang <dkssudckdah@gmail.com>
2020-06-16 17:12:46 +0900

    [Python] 모노미노도미노

--- BOJ/19235.py
+++ BOJ/19235.py
@@ -35,59 +35,61 @@ def stack(c, index, method):
 def score():
     global answer
 
+    big_flag = False
     for index in range(2):
-        for r in range(R - 1, -1, -1):
+        flag = None
+        for r in range(R):
             if EMPTY not in board[index][r]:
                 answer += 1
                 board[index][r] = [EMPTY]*C
+                flag = r
+        if flag is not None:
+            big_flag = True
+            for rr in range(flag - 1, -1, -1):
                 for c in range(C):
-                    for rr in range(r - 1, -1, -1):
-                        if board[index][rr][c] != EMPTY:
-                            temp = board[index][rr][c]
-                            shape = connect(rr, c, index, temp)
-                            if shape == 1:
-                                for rrr in range(rr + 1, R):
-                                    if board[index][rrr][c] != EMPTY:
-                                        board[index][rr][c] = EMPTY
-                                        board[index][rrr - 1][c] = temp
-                                        break
-                                else:
-                                    board[index][rr][c] = EMPTY
-                                    board[index][rrr][c] = temp
-                            elif shape == 2:
-                                if 0 <= c + dc[2] < C and board[index][rr + dr[2]][c + dc[2]] == temp:
-                                    way = -1
-                                else:
-                                    way = 1
-                                
-                                for rrr in range(rr + 1, R):
-                                    if board[index][rrr][c] != EMPTY or board[index][rrr][c + way] != EMPTY:
-                                        board[index][rr][c] = EMPTY
-                                        board[index][rr][c + way] = EMPTY
-                                        board[index][rrr - 1][c] = temp
-                                        board[index][rrr - 1][c + way] = temp
-                                        break
-                                else:
-                                    board[index][rr][c] = EMPTY
-                                    board[index][rr][c + way] = EMPTY
-                                    board[index][rrr][c] = temp
-                                    board[index][rrr][c + way] = temp
-                            else:
-                                for rrr in range(rr + 1, R):
-                                    if board[index][rrr][c] != EMPTY:
-                                        board[index][rr - 1][c] = EMPTY
-                                        board[index][rr][c] = EMPTY
-                                        board[index][rrr - 2][c] = temp
-                                        board[index][rrr - 1][c] = temp
-                                        break
-                                else:
-                                    board[index][rr - 1][c] = EMPTY
+                    if board[index][rr][c] != EMPTY:
+                        temp = board[index][rr][c]
+                        shape = connect(rr, c, index, temp)
+
+                        if shape is None:
+                            continue
+
+                        if shape == 1:
+                            for rrr in range(rr + 1, R):
+                                if board[index][rrr][c] != EMPTY:
                                     board[index][rr][c] = EMPTY
                                     board[index][rrr - 1][c] = temp
-                                    board[index][rrr][c] = temp
-                return True
-    return False
-
+                                    break
+                            else:
+                                board[index][rr][c] = EMPTY
+                                board[index][rrr][c] = temp
+                        elif shape == 2:
+                            for rrr in range(rr + 1, R):
+                                if board[index][rrr][c] != EMPTY or board[index][rrr][c + 1] != EMPTY:
+                                    board[index][rr][c] = EMPTY
+                                    board[index][rr][c + 1] = EMPTY
+                                    board[index][rrr - 1][c] = temp
+                                    board[index][rrr - 1][c + 1] = temp
+                                    break
+                            else:
+                                board[index][rr][c] = EMPTY
+                                board[index][rr][c + 1] = EMPTY
+                                board[index][rrr][c] = temp
+                                board[index][rrr][c + 1] = temp
+                        else:
+                            for rrr in range(rr + 1, R):
+                                if board[index][rrr][c] != EMPTY:
+                                    board[index][rr - 1][c] = EMPTY
+                                    board[index][rr][c] = EMPTY
+                                    board[index][rrr - 2][c] = temp
+                                    board[index][rrr - 1][c] = temp
+                                    break
+                            else:
+                                board[index][rr - 1][c] = EMPTY
+                                board[index][rr][c] = EMPTY
+                                board[index][rrr - 1][c] = temp
+                                board[index][rrr][c] = temp
+    return big_flag
 
 def pop():
     for index in range(2):
@@ -101,10 +103,12 @@ def connect(r, c, index, target):
     for x in range(4):
         rr, cc = r + dr[x], c + dc[x]
         if 0 <= rr < R and 0 <= cc < C and board[index][rr][cc] == target:
-            if x == 0 or x == 1:
+            if x == 0:
                 return 3
-            else:
+            elif x == 3:
                 return 2
+            else:
+                return None
     return 1
 
 
@@ -131,6 +135,14 @@ for tile in range(1, int(input()) + 1):
             break
 
     pop()
+
+    # print('--------------------------------------')
+    # print(t, i, j)
+    # print(*board[0], sep="\n")
+    # print()
+    # print(*list(map(list, zip(*board[1]))), sep="\n")
+    # print('--------------------------------------')
+
 
 cnt = 0
 for index in range(2):
